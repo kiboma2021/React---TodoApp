5fe0c52731f2c0794ca2effd99b8eb4e72cf04a8
kiboma <omayioben@gmail.com>
2023-11-29 15:17:18 +0300

    Add and render list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { useState } from 'react';
+
 function App() {
+  const [tasks, setTasks]=useState([
+    {id:121, name: 'Wash utensils', completed: false},
+    {id:122, name: 'Wash Clothes', completed: true},
+    {id:123, name: 'Go to the Library to collect a book', completed: false},
+    {id:124, name: 'Study python - 100 days of code', completed: false},
+    {id:125, name: 'Go swimming', completed: true},
+  ]);
   return (
     <div className="App">
       <>
         <Header />
         <div className='list-container'>
+        
+          <div>
           <h1>My to do List</h1>
+            <ul>
+              {tasks.map(task =>(
+                <li className='items'>
+                  <span>{task.id}- {task.name}</span>                
+                  <button>Delete</button>                  
+                </li>
 
+              ))}
+            </ul>
+          </div>
+          
         </div>
         <Footer />
       </>
